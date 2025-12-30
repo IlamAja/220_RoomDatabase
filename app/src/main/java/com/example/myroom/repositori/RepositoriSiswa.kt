652ec87f560195afba6d5b87622de5e864a8259d
Ilam Ball <informasiilam@gmail.com>
2025-12-31 00:22:37 +0700

    membuat peta navigasi

--- app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
@@ -7,7 +7,9 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
+    fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun insertSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -16,5 +18,9 @@ class OfflineRepositoriSiswa(
 
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
 
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
