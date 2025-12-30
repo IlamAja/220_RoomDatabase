4286bd51e93fd052d17e518b4a5531291c0b0544
Ilam Ball <informasiilam@gmail.com>
2025-12-31 00:23:12 +0700

    membuat Data Access Object (DAO) untuk Siswa

--- app/src/main/java/com/example/myroom/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroom/room/SiswaDao.kt
@@ -1,4 +1,24 @@
 package com.example.myroom.room
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface SiswaDao {
+
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa?>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
